6ad55ea9edeb97ab29e6c59502290cd4df11120d
Kevin Min <kevin.b.min@gmail.com>
2024-11-15 19:41:04 -0500

    Cleaning up HDMI code (currently broken)

--- hdl/ir_decoder.sv
+++ hdl/ir_decoder.sv
@@ -1,128 +1,128 @@
-`timescale 1ns / 1ps
-`default_nettype none
-module ir_decoder
-       #( parameter SBD  = 900_000, //sync burst duration
-          parameter SSD  = 450_000, //sync silence duration
-          parameter BBD = 60_000, //bit burst duration
-          parameter BSD0 = 60_000, //bit silence duration (for 0)
-          parameter BSD1 = 160_000, //bit silence duration (for 1)
-          parameter MARGIN = 20_000 //The +/- of your signals
-          // the duration is in terms of number of cycles per each duration
-        )
-        ( input wire clk_in, //clock in (100MHz)
-          input wire rst_in, //reset in
-          input wire signal_in, //signal in
-          output logic [31:0] code_out, //where to place 32 bit code once captured TODO change to 30 bit
-          output logic new_code_out, //single-cycle indicator that new code is present!
-          output logic [2:0] error_out, //output error codes for debugging
-          output logic [3:0] state_out //current state out (helpful for debugging)
-        );
+// `timescale 1ns / 1ps
+// `default_nettype none
+// module ir_decoder
+//        #( parameter SBD  = 900_000, //sync burst duration
+//           parameter SSD  = 450_000, //sync silence duration
+//           parameter BBD = 60_000, //bit burst duration
+//           parameter BSD0 = 60_000, //bit silence duration (for 0)
+//           parameter BSD1 = 160_000, //bit silence duration (for 1)
+//           parameter MARGIN = 20_000 //The +/- of your signals
+//           // the duration is in terms of number of cycles per each duration
+//         )
+//         ( input wire clk_in, //clock in (100MHz)
+//           input wire rst_in, //reset in
+//           input wire signal_in, //signal in
+//           output logic [31:0] code_out, //where to place 32 bit code once captured TODO change to 30 bit
+//           output logic new_code_out, //single-cycle indicator that new code is present!
+//           output logic [2:0] error_out, //output error codes for debugging
+//           output logic [3:0] state_out //current state out (helpful for debugging)
+//         );
 
-  logic last_signal;
-  logic [31:0] signal_counter;
-  typedef enum {IDLE=0, SL=1, SH=2, DL=3, DH=4, F0=5, F1=6, CHECK=7,DATA=8} ir_state;
+//   logic last_signal;
+//   logic [31:0] signal_counter;
+//   typedef enum {IDLE=0, SL=1, SH=2, DL=3, DH=4, F0=5, F1=6, CHECK=7,DATA=8} ir_state;
  
-  current_counter mcc( .clk_in(clk_in),
-                        .rst_in(rst_in),
-                        .signal_in(signal_in),
-                        .tally_out(signal_counter));
+//   current_counter mcc( .clk_in(clk_in),
+//                         .rst_in(rst_in),
+//                         .signal_in(signal_in),
+//                         .tally_out(signal_counter));
  
-  ir_state state; //state of system!
-  logic [31:0] data_buffer; // buffer for the 32 bits of data
-  logic [5:0] num_bits_so_far;
+//   ir_state state; //state of system!
+//   logic [31:0] data_buffer; // buffer for the 32 bits of data
+//   logic [5:0] num_bits_so_far;
  
-  assign state_out = state;
+//   assign state_out = state;
  
-  always_ff @(posedge clk_in)begin
-    if (rst_in)begin
-      state <= IDLE;
-      code_out <= 0;
-      new_code_out <= 0;
-      error_out <=0;
-      data_buffer <= 0;
-      num_bits_so_far <= 0;
-    end else begin
-      case (state)
-      // TODO do we do in terms of the current signal or the last signal
-        IDLE : begin
-          state <= signal_in == 1'b0 ? SL : IDLE; // move to sync low if a burst of light is detected
-          data_buffer <= 0; // clear the data buffer
-          num_bits_so_far <= 0;
-          code_out <= 0;
-          new_code_out <= 0;
-          error_out <= 0;
-        end
+//   always_ff @(posedge clk_in)begin
+//     if (rst_in)begin
+//       state <= IDLE;
+//       code_out <= 0;
+//       new_code_out <= 0;
+//       error_out <=0;
+//       data_buffer <= 0;
+//       num_bits_so_far <= 0;
+//     end else begin
+//       case (state)
+//       // TODO do we do in terms of the current signal or the last signal
+//         IDLE : begin
+//           state <= signal_in == 1'b0 ? SL : IDLE; // move to sync low if a burst of light is detected
+//           data_buffer <= 0; // clear the data buffer
+//           num_bits_so_far <= 0;
+//           code_out <= 0;
+//           new_code_out <= 0;
+//           error_out <= 0;
+//         end
 
-        // sync low
-        SL : begin
-          // waiting to see if its the appropriate length
-          state <= signal_in == 1'b1 
-                    ? (signal_counter >= (SBD - MARGIN) && signal_counter <= (SBD + MARGIN)) 
-                        ? SH // appropriate length go to sync high
-                        : IDLE // signal not long enough, go to IDLE
-                    : signal_counter > (SBD + MARGIN)
-                      ? IDLE // signal too long
-                      : SL; // signal still low
-        end
+//         // sync low
+//         SL : begin
+//           // waiting to see if its the appropriate length
+//           state <= signal_in == 1'b1 
+//                     ? (signal_counter >= (SBD - MARGIN) && signal_counter <= (SBD + MARGIN)) 
+//                         ? SH // appropriate length go to sync high
+//                         : IDLE // signal not long enough, go to IDLE
+//                     : signal_counter > (SBD + MARGIN)
+//                       ? IDLE // signal too long
+//                       : SL; // signal still low
+//         end
 
-        // sync high
-        SH : begin
-          state <= signal_in == 1'b0
-                    ? (signal_counter >= (SSD - MARGIN) && signal_counter <= (SSD + MARGIN)) 
-                        ? DL // appropriate length go to "data low"
-                        : IDLE // signal not long enough, go to IDLE
-                    : signal_counter > (SSD + MARGIN)
-                      ? IDLE // signal too long
-                      : SH; // signal still high
-        end
+//         // sync high
+//         SH : begin
+//           state <= signal_in == 1'b0
+//                     ? (signal_counter >= (SSD - MARGIN) && signal_counter <= (SSD + MARGIN)) 
+//                         ? DL // appropriate length go to "data low"
+//                         : IDLE // signal not long enough, go to IDLE
+//                     : signal_counter > (SSD + MARGIN)
+//                       ? IDLE // signal too long
+//                       : SH; // signal still high
+//         end
 
-        DL : begin
-          state <= signal_in == 1'b1
-                  ? (signal_counter >= (BBD - MARGIN) && signal_counter <= (BBD + MARGIN)) 
-                        ? DH // appropriate length go to "data high"
-                        : IDLE // signal not long enough, go to IDLE
-                    : signal_counter > (BBD + MARGIN)
-                      ? IDLE // signal too long
-                      : DL; // signal still low
-        end
+//         DL : begin
+//           state <= signal_in == 1'b1
+//                   ? (signal_counter >= (BBD - MARGIN) && signal_counter <= (BBD + MARGIN)) 
+//                         ? DH // appropriate length go to "data high"
+//                         : IDLE // signal not long enough, go to IDLE
+//                     : signal_counter > (BBD + MARGIN)
+//                       ? IDLE // signal too long
+//                       : DL; // signal still low
+//         end
 
-        DH : begin
-          state <= signal_in == 1'b0
-                  ? (signal_counter >= (BSD0 - MARGIN) && signal_counter <= (BSD0 + MARGIN)) 
-                        ? F0 // appropriate length for a '0' go to "F0"
-                        : (signal_counter >= (BSD1 - MARGIN) && signal_counter <= (BSD1 + MARGIN)) 
-                          ? F1 // appropriate length for a '1' go to "F1"
-                          : IDLE// signal not the right length, go to IDLE
-                    : signal_counter > (BSD1 + MARGIN)
-                      ? IDLE // signal too long
-                      : DH; // signal still high
-        end
+//         DH : begin
+//           state <= signal_in == 1'b0
+//                   ? (signal_counter >= (BSD0 - MARGIN) && signal_counter <= (BSD0 + MARGIN)) 
+//                         ? F0 // appropriate length for a '0' go to "F0"
+//                         : (signal_counter >= (BSD1 - MARGIN) && signal_counter <= (BSD1 + MARGIN)) 
+//                           ? F1 // appropriate length for a '1' go to "F1"
+//                           : IDLE// signal not the right length, go to IDLE
+//                     : signal_counter > (BSD1 + MARGIN)
+//                       ? IDLE // signal too long
+//                       : DH; // signal still high
+//         end
 
-        F0 : begin
-          data_buffer <= {data_buffer[30:0], 1'b0}; // TODO make sure this is msb
-          num_bits_so_far <= num_bits_so_far + 1;
-          state <= CHECK;
-        end
+//         F0 : begin
+//           data_buffer <= {data_buffer[30:0], 1'b0}; // TODO make sure this is msb
+//           num_bits_so_far <= num_bits_so_far + 1;
+//           state <= CHECK;
+//         end
 
-        F1 : begin
-          data_buffer <= {data_buffer[30:0], 1'b1};
-          num_bits_so_far <= num_bits_so_far + 1;
-          state <= CHECK;
-        end
+//         F1 : begin
+//           data_buffer <= {data_buffer[30:0], 1'b1};
+//           num_bits_so_far <= num_bits_so_far + 1;
+//           state <= CHECK;
+//         end
 
-        CHECK : begin
-          // check if there's been 32 bits into the data buffer
-          state <= num_bits_so_far == 32 ? DATA : DL; 
-        end
+//         CHECK : begin
+//           // check if there's been 32 bits into the data buffer
+//           state <= num_bits_so_far == 32 ? DATA : DL; 
+//         end
 
-        DATA : begin
-          new_code_out <= 1;
-          code_out <= data_buffer;
-          state <= IDLE;
-        end
-      endcase
-    end
-  end
+//         DATA : begin
+//           new_code_out <= 1;
+//           code_out <= data_buffer;
+//           state <= IDLE;
+//         end
+//       endcase
+//     end
+//   end
  
-endmodule
-`default_nettype none
+// endmodule
+// `default_nettype none
---
+++ hdl/text_display.sv
@@ -0,0 +1,213 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+module image_sprite (
+    input wire clk_in,
+    input wire sys_rst_pixel,
+    input wire data_valid_in,
+    input wire [4:0] data_in,
+    output logic tmds_red,
+    output logic tmds_green,
+    output logic tmds_blue
+    );
+
+
+    // Clock and Reset Signals
+    logic          clk_camera;
+    logic          clk_pixel;
+    logic          clk_5x;
+    logic          clk_xc;
+
+    logic          clk_100_passthrough;
+
+    // clocking wizards to generate the clock speeds we need for our different domains
+    // clk_camera: 200MHz, fast enough to comfortably sample the cameera's PCLK (50MHz)
+    cw_hdmi_clk_wiz wizard_hdmi
+        (.sysclk(clk_100_passthrough),
+        .clk_pixel(clk_pixel),
+        .clk_tmds(clk_5x),
+        .reset(0));
+
+    cw_fast_clk_wiz wizard_migcam
+        (.clk_in1(clk_in),
+        .clk_camera(clk_camera),
+        .clk_xc(clk_xc),
+        .clk_100(clk_100_passthrough),
+        .reset(0));
+
+    logic [4:0] text [511:0];
+    logic [8:0] counter;
+
+    always_ff @(posedge clk_in) begin
+        if(sys_rst_pixel) begin
+            counter <= 0;
+            for(int i = 0; i < 512; i = i + 1) begin
+                text[i] <= 26;
+            end
+        end else if(data_valid_in) begin
+            text[counter] <= data_in;
+            counter <= counter + 1;
+        end
+    end
+  
+    evt_counter cntr
+        (.clk_in(clk_in),
+        .rst_in(sys_rst_pixel),
+        .evt_in(data_valid_in),
+        .count_out(counter)
+        )
+
+    // video signal generator signals
+    logic          hsync_hdmi;
+    logic          vsync_hdmi;
+    logic [10:0]   hcount_hdmi;
+    logic [9:0]    vcount_hdmi;
+    logic          active_draw_hdmi;
+    logic [5:0]    frame_count_hdmi;
+    logic          nf_hdmi;
+
+
+
+    // rgb output values
+    logic [7:0]          red,green,blue;
+
+    logic [10:0] hcount_pipe [4:0];
+    logic [9:0] vcount_pipe [4:0];
+    logic [10:0] hcount_left_pipe [4:0];
+    logic [9:0] vcount_up_pipe [4:0];
+    logic [10:0] hcount_pipe_2 [4:0];
+    logic [9:0] vcount_pipe_2 [4:0];
+    logic          hsync_hdmi_pipe [8:0];
+    logic          vsync_hdmi_pipe [8:0];
+    logic          active_draw_hdmi_pipe [8:0];
+
+    always_ff @(posedge clk_pixel)begin
+        hcount_pipe[0] <= hcount_hdmi;
+        vcount_pipe[0] <= vcount_hdmi;
+        hsync_hdmi_pipe[0] <= hsync_hdmi;
+        vsync_hdmi_pipe[0] <= vsync_hdmi;
+        active_draw_hdmi_pipe[0] <= active_draw_hdmi;
+        if(hcount_hdmi >= 640) begin
+            hcount_left_pipe[0] <= 16;
+            hcount_pipe_2[0] <= hcount_hdmi - 640;
+        end else begin
+            hcount_left_pipe[0] <= 0;
+            hcount_pipe_2[0] <= hcount_hdmi;
+        end
+        if(vcount_hdmi >= 360) begin
+            vcount_up_pipe[0] <= 8;
+            vcount_pipe_2[0] <= vcount_hdmi - 360;
+        end else begin
+            vcount_up_pipe[0] <= 0;
+            vcount_pipe_2[0] <= vcount_hdmi;
+        end
+        for (int i = 1; i <= 8; i = i+1)begin
+            hsync_hdmi_pipe[i] <= hsync_hdmi_pipe[i - 1];
+            vsync_hdmi_pipe[i] <= vsync_hdmi_pipe[i - 1];
+            active_draw_hdmi_pipe[i] <= active_draw_hdmi_pipe[i - 1];
+        end
+        for (int i = 1; i <= 4; i = i+1)begin
+            hcount_pipe[i] <= hcount_pipe[i - 1];
+            vcount_pipe[i] <= vcount_pipe[i - 1];
+            if(hcount_pipe_2[i - 1] >= (640 >> i)) begin
+                hcount_left_pipe[i] <= hcount_left_pipe[i - 1] + (16 >> i);
+                hcount_pipe_2[i] <= hcount_pipe_2[i - 1] - (640 >> i);
+            end else begin
+                hcount_left_pipe[i] <= hcount_left_pipe[i - 1];
+                hcount_pipe_2[i] <= hcount_pipe_2[i - 1];
+            end
+            end
+            for (int i = 1; i <= 3; i = i+1)begin
+            if(vcount_pipe_2[i - 1] >= (360 >> i)) begin
+                vcount_up_pipe[i] <= vcount_up_pipe[i - 1] + (8 >> i);
+                vcount_pipe_2[i] <= vcount_pipe_2[i - 1] - (360 >> i);
+            end else begin
+                vcount_up_pipe[i] <= vcount_up_pipe[i - 1];
+                vcount_pipe_2[i] <= vcount_pipe_2[i - 1];
+            end
+        end
+        vcount_pipe[4] <= vcount_pipe[3];
+        vcount_up_pipe[4] <= vcount_up_pipe[3];
+    end
+
+    // width 228, height 225 for 6x5 grid
+    // width 38, height 45 per letter
+    // if I do 40x45, that perfectly fits 32x16 letters in a 1280x720 display
+    image_sprite #(
+        .WIDTH(228),
+        .HEIGHT(225))
+        letter_sprite (
+        .pixel_clk_in(clk_pixel),
+        .rst_in(sys_rst_pixel),
+        .hcount_in(hcount_pipe[4]),  
+        .vcount_in(vcount_pipe[4]),  
+        .x_in(hcount_left_pipe[4] * 40),
+        .y_in(vcount_up_pipe[4] * 45),
+        .letter(text[hcount_left_pipe[4] + vcount_up_pipe[4] * 32]),
+        .red_out(red),
+        .green_out(green),
+        .blue_out(blue));
+
+    // HDMI video signal generator
+    video_sig_gen vsg
+        (
+        .pixel_clk_in(clk_pixel),
+        .rst_in(sys_rst_pixel),
+        .hcount_out(hcount_hdmi),
+        .vcount_out(vcount_hdmi),
+        .vs_out(vsync_hdmi),
+        .hs_out(hsync_hdmi),
+        .nf_out(nf_hdmi),
+        .ad_out(active_draw_hdmi),
+        .fc_out(frame_count_hdmi)
+        );
+
+
+   logic [9:0] tmds_10b [0:2];
+
+   tmds_encoder tmds_red(
+       .clk_in(clk_pixel),
+       .rst_in(sys_rst_pixel),
+       .data_in(red),
+       .control_in(2'b0),
+       .ve_in(active_draw_hdmi_pipe[8]),
+       .tmds_out(tmds_10b[2]));
+
+   tmds_encoder tmds_green(
+         .clk_in(clk_pixel),
+         .rst_in(sys_rst_pixel),
+         .data_in(green),
+         .control_in(2'b0),
+         .ve_in(active_draw_hdmi_pipe[8]),
+         .tmds_out(tmds_10b[1]));
+
+   tmds_encoder tmds_blue(
+        .clk_in(clk_pixel),
+        .rst_in(sys_rst_pixel),
+        .data_in(blue),
+        .control_in({vsync_hdmi_pipe[8],hsync_hdmi_pipe[8]}),
+        .ve_in(active_draw_hdmi_pipe[8]),
+        .tmds_out(tmds_10b[0]));
+
+        
+
+    //three tmds_serializers (blue, green, red):
+    tmds_serializer red_ser(
+         .clk_pixel_in(clk_pixel),
+         .clk_5x_in(clk_5x),
+         .rst_in(sys_rst_pixel),
+         .tmds_in(tmds_10b[2]),
+         .tmds_out(tmds_red));
+    tmds_serializer green_ser(
+         .clk_pixel_in(clk_pixel),
+         .clk_5x_in(clk_5x),
+         .rst_in(sys_rst_pixel),
+         .tmds_in(tmds_10b[1]),
+         .tmds_out(tmds_green));
+    tmds_serializer blue_ser(
+         .clk_pixel_in(clk_pixel),
+         .clk_5x_in(clk_5x),
+         .rst_in(sys_rst_pixel),
+         .tmds_in(tmds_10b[0]),
+         .tmds_out(tmds_blue));
+endmodule
