47b8f012b5ae8f857b0f756bf4c78e64ed656a63
Anna Simmons <annasim@mit.edu>
2024-11-21 16:12:05 -0500

    updated ir transmitter and reciever timing and 40khz led frequency

--- hdl/ir_transmitter.sv
+++ hdl/ir_transmitter.sv
@@ -1,11 +1,10 @@
 `timescale 1ns / 1ps
 `default_nettype none
 module ir_transmitter
-       #( parameter SBD  = 900_000, //sync burst duration
-          parameter SSD  = 450_000, //sync silence duration
-          parameter BBD = 60_000, //bit burst duration
-          parameter BSD0 = 60_000, //bit silence duration (for 0)
-          parameter BSD1 = 160_000, //bit silence duration (for 1)
+       #( parameter SBD  = 240_000, //sync burst duration
+          parameter BSD = 60_000, //bit burst duration
+          parameter BBD0 = 60_000, //bit silence duration (for 0)
+          parameter BBD1 = 120_000, //bit silence duration (for 1)
           parameter MARGIN = 20_000, //The +/- of your signals
           parameter MESSAGE_LENGTH = 30
           // the duration is in terms of number of cycles per each duration
@@ -20,12 +19,23 @@ module ir_transmitter
     logic [31:0] cur_cycle_counter;
     logic [$clog2(MESSAGE_LENGTH):0] cur_bit_counter;
     logic [MESSAGE_LENGTH-1:0] cur_data;
+
+    // logic signal_value;
+    logic signal_high;
+    logic signal_low;
+    localparam DC_LENGTH = 1280;
     
-    typedef enum {IDLE = 0, SB = 1, SS = 2, BB = 3, BS0 = 4, BS1 = 5, EOM = 6} ir_state;
-    ir_state state; //state of system!
+    assign signal_low = 0;
 
 
-// TODO 40kHz clock cycle use pulses
+    pwm ir_pwm (.clk_in(clk_in),
+            .rst_in(rst_in),
+            .dc_in(DC_LENGTH),
+            .sig_out(signal_high));
+    
+    typedef enum {IDLE = 0, SB = 1, SS = 2, BS = 3, BB0 = 4, BB1 = 5, EOM = 6} ir_state;
+    ir_state state; //state of system
+
     
     always_ff @(posedge clk_in) begin
         if (rst_in) begin
@@ -33,7 +43,7 @@ module ir_transmitter
             state <= IDLE;
             cur_cycle_counter <= 0;
             cur_bit_counter <= 0;
-            signal_out <= 0;
+            signal_out <= signal_low;
             cur_data = 0;
         end
         else begin
@@ -44,88 +54,84 @@ module ir_transmitter
                         busy_out <= 1;
                         state <= SB;
                         cur_cycle_counter <= 0;
-                        signal_out <= 1;
+                        signal_out <= signal_high;
                     end
                     else begin
                         signal_out <= 0;
                     end
                 end
 
-                // synch burst
+                // sync burst
                 SB : begin
                     if (cur_cycle_counter < SBD) begin
                         cur_cycle_counter <= cur_cycle_counter + 1;
-                        signal_out <= 1;
+                        signal_out <= signal_high;
                     end
                     else begin
                         cur_cycle_counter <= 0;
-                        state <= SS;
-                        signal_out <= 0;
+                        state <= BS;
+                        signal_out <= signal_low;
                     end
                 end
 
-                // synch silence
-                SS : begin
-                    if (cur_cycle_counter < SSD) begin
+                // bit silence 0/1
+                BS : begin
+                    if (cur_cycle_counter < BSD) begin
                         cur_cycle_counter <= cur_cycle_counter + 1;
-                        signal_out <= 0;
-                    end
-                    else begin
-                        cur_cycle_counter <= 0;
-                        state <= BB;
-                        signal_out <= 1;
-                    end
-                end
-
-                // bit burst 0/1
-                BB : begin
-                    if (cur_cycle_counter < BBD) begin
-                        cur_cycle_counter <= cur_cycle_counter + 1;
-                        signal_out <= 1;
+                        signal_out <= signal_low;
                     end
                     else begin
                         cur_cycle_counter <= 0;
                         cur_bit_counter <= cur_bit_counter + 1;
-                        state <= cur_data[MESSAGE_LENGTH-cur_bit_counter-1]  == 0 ? BS0 : BS1; // MSB first
-                        signal_out <= 0;
+                        state <= cur_data[MESSAGE_LENGTH-cur_bit_counter-1]  == 0 ? BB0 : BB1; // MSB first
+                        signal_out <= signal_high;
                     end
                 end
 
-                // bit silence 0
-                BS0 :  begin
-                    if (cur_cycle_counter < BSD0) begin
+                // bit burst 0
+                BB0 :  begin
+                    if (cur_cycle_counter < BBD0) begin
                         cur_cycle_counter <= cur_cycle_counter + 1;
-                        signal_out <= 0;
+                        signal_out <= signal_high;
                     end
                     else begin
                         cur_cycle_counter <= 0;
-                        state <= cur_bit_counter == MESSAGE_LENGTH ? EOM : BB;
-                        signal_out <= 1;
+                        state <= cur_bit_counter == MESSAGE_LENGTH ? EOM : BS;
+                        signal_out <= signal_low;
                     end
 
                 end
 
-                // bit silence 1
-                BS1 : begin
-                    if (cur_cycle_counter < BSD1) begin
+                // bit burst 1
+                BB1 : begin
+                    if (cur_cycle_counter < BBD1) begin
                         cur_cycle_counter <= cur_cycle_counter + 1;
-                        signal_out <= 0;
+                        signal_out <= signal_high;
                     end
                     else begin
                         cur_cycle_counter <= 0;
-                        state <= cur_bit_counter == MESSAGE_LENGTH ? EOM : BB;
-                        signal_out <= 1;
+                        state <= cur_bit_counter == MESSAGE_LENGTH ? EOM : BS;
+                        signal_out <= signal_low;
                     end
 
                 end
 
                 // end of message single cycle to return to idle 
                 EOM : begin
-                    busy_out <= 0;
-                    state <= IDLE;
-                    cur_bit_counter <= 0;
-                    cur_cycle_counter <= 0;
-                    signal_out <= 0;
+
+                    if (cur_cycle_counter < BSD) begin
+                        cur_cycle_counter <= cur_cycle_counter + 1;
+                        signal_out <= signal_low;
+                    end
+                    else begin
+                        cur_cycle_counter <= 0;
+                        cur_bit_counter <= cur_bit_counter + 1;
+                        state <= IDLE;
+                        signal_out <= signal_low;
+                        busy_out <= 0;
+                        cur_bit_counter <= 0;
+                        cur_cycle_counter <= 0;
+                    end
                 end
 
 
