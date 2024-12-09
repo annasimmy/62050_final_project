de2d7d2e4b17f111a3ccd2ac1ac8d70a5b711854
Kevin Min <kevin.b.min@gmail.com>
2024-12-09 15:03:54 -0500

    Bombe module partial progress

--- hdl/bombe_module.sv
+++ hdl/bombe_module.sv
@@ -1,10 +1,14 @@
 module bombe_module (
 	input wire clk_in,
-  input wire rst_in
-  // Todo: Input/output needs fixing
+  input wire rst_in,
+  input [4:0] coded_letter,
+  input [4:0] crib_in [15:0],
+  output [8:0] rotor_select_out,
+  output [14:0] rotor_initial_out,
+  output rotor_valid_out
   );
 
-  
+  // Todo: Better way of passing in coded message (BRAM)
   logic [4:0] coded_message [1023:0];
 
   // Should only go from 0 to 12 - the extra x2 factors come from the for loop of 32 simultaneous Enigmas
@@ -15,76 +19,96 @@ module bombe_module (
   logic [2:0] rotor_1_num;
   // rotor_2_num missing (4 possibilities) - again comes from the for loop
   // Should go 0 to 4
-  logic [1:0] rotor_3_num; 
+  logic [2:0] rotor_3_num; 
 
   // true if ith enigma is done checking current settings
-  logic checked_curr [31:0];
+  logic [31:0] checked_curr;
+  logic [31:0] ready_curr;
 
+  logic [4:0] enigma_valid [31:0];
   logic [4:0] enigma_output [31:0];
 
   // Letter we are currently looking at in message
   logic [9:0] letter_pos;
 
-  // always_ff @(posedge clk_in) begin
-  //   if(rst_in) begin
-      
-  //   end else if(~(|checked_curr)) begin
-  //     if(rotor_3_initial == 12) begin
-  //       rotor_3_initial <= 0;
-  //       if(rotor_2_initial == 12) begin
-  //         rotor_2_initial <= 0;
-  //         if(rotor_1_initial == 12) begin
-  //           rotor_1_initial <= 0;
-            
-  //           // Todo 
-  //           // if(rotor_3_num == 2) begin
-  //           //   rotor_3_initial <= 0;
-  //           // end else begin
-  //           //   rotor_3_initial <= rotor_3_initial + 1;
-  //           // end
+  // High when rotor settings are valid
+  logic rotor_valid;
 
-  //         end else begin
-  //           rotor_1_initial <= rotor_1_initial + 1;
-  //         end
-  //       end else begin
-  //         rotor_2_initial <= rotor_2_initial + 1;
-  //       end
-  //     end else begin
-  //       rotor_3_initial <= rotor_3_initial + 1;
-  //     end
-  //     if(rotor_1_num != rotor_3_num) begin
-  //       for(int i = 0; i < 32; i = i + 1) begin
-  //         checked_curr[i] <= 0;
-  //       end
-  //     end
-  //   end else begin
-  //     // for(int i = 0; i < 32; i = i + 1) begin
-  //     //   enigma curr_enigma
-  //     //     (
-  //     //     .clk_in(clk_in),
-  //     //     .rst_in(rst_in),
-  //     //     .rotor_select({
-  //     //       rotor_1_num, 
-  //     //       // Todo: Needs fixing
-  //     //       (i[1:0] >= rotor_1_num ? (logic[2:0]'i[1:0]) + 1 : (logic[2:0]'i[1:0])),
-  //     //       rotor_3_num,
-  //     //     }),
-  //     //     .rotor_initial({
-  //     //       ((rotor_1_initial << 1) + i[2:2]),
-  //     //       ((rotor_2_initial << 1) + i[3:3]),
-  //     //       ((rotor_3_initial << 1) + i[4:4]),
-  //     //     }),
-  //     //     .data_valid_in(),
-  //     //     .data_in(),
-  //     //     .data_out(enigma_output[i])
-  //     //     );
-  //     //   // Todo: Add output == input in this if statement
-  //     //   if(enigma_output[i] != coded_message[i]) begin
-  //     //     checked_curr[i] <= 1;
-  //     //   end
-  //     // end
+  genvar i;
+  generate 
+    for(i = 0; i < 32; i = i + 1) begin
+      enigma curr_enigma
+        (
+        .clk_in(clk_in),
+        .rst_in(rst_in),
+        .rotor_select({
+          rotor_1_num, 
+          ((i[1:0] >= rotor_1_num) ? (i[1:0] == 3 : 3'b100 : {0, i[1:0]}) : {0, i[1:0]}),
+          rotor_3_num
+        }),
+        .rotor_initial({
+          ((rotor_1_initial << 1) + i[2:2]),
+          ((rotor_2_initial << 1) + i[3:3]),
+          ((rotor_3_initial << 1) + i[4:4])
+        }),
+        .data_valid_in(&ready_curr),
+        .rotor_valid_in(rotor_valid),
+        .data_in(coded_message[letter_pos]),
+        .ready(ready_curr[i]),
+        .data_valid_out(enigma_valid[i]),
+        .data_out(enigma_output[i])
+        );
+    end
+  endgenerate
 
-    
-  //   end
-  // end
+
+  always_ff @(posedge clk_in) begin
+    if(rst_in) begin
+      rotor_valid <= 0;
+      rotor_1_initial <= 0;
+      rotor_3_initial <= 0;
+      rotor_3_num <= 0;
+      rotor_1_num <= 0;
+    end else if(&ready_curr && letter_pos == 1023) begin
+      if(rotor_3_initial == 12) begin
+        rotor_3_initial <= 0;
+        if(rotor_2_initial == 12) begin
+          rotor_2_initial <= 0;
+          if(rotor_1_initial == 12) begin
+            rotor_1_initial <= 0;
+            if(rotor_3_num == 4) begin
+              rotor_3_num <= 0;
+              if(rotor_1_num == 4) begin
+                // TODO: Bombe is done running, end
+              end else begin
+                rotor_1_num <= rotor_1_num + 1;
+              end
+            end else begin
+              rotor_3_num <= rotor_3_num + 1;
+            end
+
+          end else begin
+            rotor_1_initial <= rotor_1_initial + 1;
+          end
+        end else begin
+          rotor_2_initial <= rotor_2_initial + 1;
+        end
+      end else begin
+        rotor_3_initial <= rotor_3_initial + 1;
+      end
+      if(rotor_1_num != rotor_3_num) begin
+        for(int i = 0; i < 32; i = i + 1) begin
+          checked_curr[i] <= 0;
+        end
+        rotor_valid <= 1;
+      end
+    end else if(rotor_valid) begin
+      rotor_valid <= 0;
+    end
+    end else if(&ready_curr) begin
+      // Todo: Check if the last 16 outputs of enigma_output[i] match the crib, if so then output it as a valid rotor setting
+      // Else do nothing
+      letter_pos <= letter_pos + 1;
+    end
+  end
 endmodule
